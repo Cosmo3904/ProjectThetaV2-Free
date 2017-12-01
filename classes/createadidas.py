3813883a3646a5e6dd6e6fedae70e8273e282941
Cosmo3904 <bgbeach@gtcc.edu>
2017-12-01 12:13:57 -0500

    Update createadidas.py

--- classes/createadidas.py
+++ classes/createadidas.py
@@ -43,7 +43,10 @@ def createv2(first, last, email, password):
             '__fp' : soup.find('input', {'name':'__fp'})['value']
         }
         res = s.post('https://shop.miteam.adidas.us/miadidas-miteam/Login.action', data = payload, headers = headers)
-        return(True)
+        if login(email, password):
+            return(True)
+        else:
+            return(False)
     except:
         return(False)
 
@@ -114,6 +117,69 @@ def createaccount(first, last, email, password):
         res = s.get('https://cp.adidas.com/web/ssoCookieCreate?' + resumeurldata + '&' + resumeurldata2, headers = headers)
         res = s.get(geturl)
         res = s.get('https://cp.adidas.com' + resumeurl)
+        if login(email, password):
+            return(True)
+        else:
+            return(False)
+    except:
+        return(False)
+
+def login(email, passw):
+    try:
+        s = requests.Session()
+        s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
+        headers = {
+          'Origin':'https://cp.adidas.com',
+          'Referer':'https://cp.adidas.com',
+          'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+        }
+        res = s.get('https://cp.adidas.com/web/eCom/en_US/loadsignin?target=account', headers = headers)
+        soup = bs(res.text, 'lxml')
+        payload = {
+            'username': email,
+            'password': passw,
+            'signinSubmit': 'Sign in'
+        }
+        payload['IdpAdapterId'] = soup.find('input', {'name':'IdpAdapterId'})['value']
+        payload['SpSessionAuthnAdapterId'] = soup.find('input', {'name':'SpSessionAuthnAdapterId'})['value']
+        payload['PartnerSpId'] = soup.find('input', {'name':'PartnerSpId'})['value']
+        payload['remembermeParam'] = soup.find('input', {'name':'remembermeParam'})['value']
+        payload['validator_id'] = soup.find('input', {'name':'validator_id'})['value']
+        payload['TargetResource'] = soup.find('input', {'name':'TargetResource'})['value']
+        payload['InErrorResource'] = soup.find('input', {'name':'InErrorResource'})['value']
+        payload['loginUrl'] = soup.find('input', {'name':'loginUrl'})['value']
+        payload['cd'] = soup.find('input', {'name':'cd'})['value']
+        payload['app'] = soup.find('input', {'name':'app'})['value']
+        payload['locale'] = soup.find('input', {'name':'locale'})['value']
+        payload['domain'] = soup.find('input', {'name':'domain'})['value']
+        payload['email'] = soup.find('input', {'name':'email'})['value']
+        payload['pfRedirectBaseURL_test'] = soup.find('input', {'name':'pfRedirectBaseURL_test'})['value']
+        payload['pfStartSSOURL_test'] = soup.find('input', {'name':'pfStartSSOURL_test'})['value']
+        payload['resumeURL_test'] = soup.find('input', {'name':'resumeURL_test'})['value']
+        payload['FromFinishRegistraion'] = soup.find('input', {'name':'FromFinishRegistraion'})['value']
+        payload['CSRFToken'] = soup.find('input', {'name':'CSRFToken'})['value']
+        res = s.post('https://cp.adidas.com/idp/startSSO.ping', data = payload, headers = headers)
+        sub = res.text[res.text.find('/idp/')+5:]
+        firststring = sub[:sub.find('/idp/')]
+        secondstring = sub[sub.find('/idp/')+5:sub.find("'")]
+        url = '/idp/' + firststring + '/idp/' + secondstring
+        res = s.get('https://cp.adidas.com/web/ssoCookieCreate?resume={}&cd={}|{}|{}|null'.format(url.replace(' ', '%7C'), payload['cd'].replace(' ', '%7C'), payload['locale'].replace(' ', '%7C'), payload['domain'].replace(' ', '%7C')), headers = headers)
+        #soup = bs(res.text, 'lxml')
+        #nexturl = soup.find('img')['src']
+        #self.s.get(nexturl)
+        res = s.get('https://cp.adidas.com' + url, headers = headers)
+        soup = bs(res.text, 'lxml')
+        payload = {
+            'RelayState':soup.find('input', {'name':'RelayState'})['value'],
+            'SAMLResponse':soup.find('input', {'name': 'SAMLResponse'})['value']
+        }
+        res = s.post('https://cp.adidas.com/sp/ACS.saml2', data = payload, headers = headers)
+        soup = bs(res.text, 'lxml')
+        payload = {
+            'REF':soup.find('input', {'name':'REF'})['value'],
+            'TargetResource':soup.find('input', {'name':'TargetResource'})['value']
+        }
+        res = s.post('https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-ResumeLogin', headers = headers, data = payload)
         return(True)
     except:
         return(False)
