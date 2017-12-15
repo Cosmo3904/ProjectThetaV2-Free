f1e266b76f79f6d881165f6719d230835e3bbdd4
Cosmo3904 <bgbeach@gtcc.edu>
2017-12-15 08:45:42 -0500

    updated accounts creation to be done on GB locale

--- classes/createadidas.py
+++ classes/createadidas.py
@@ -66,19 +66,20 @@ def createaccount(first, last, email, password):
             'Content-Length':'549',
             'Content-Type':'application/x-www-form-urlencoded',
             'DNT':'1',
-            'Host':'cp.adidas.com',
-            'Origin':'https://cp.adidas.com',
-            'Referer':'https://cp.adidas.com/web/eCom/en_US/accountcreate',
+            'Host':'cp.adidas.co.uk',
+            'Origin':'https://cp.adidas.co.uk',
+            'Referer':'https://cp.adidas.co.uk/web/eCom/en_GB/accountcreate',
             'Upgrade-Insecure-Requests':'1'
         }
-        res = s.get('https://cp.adidas.com/web/eCom/en_US/loadcreateaccount')
+        res = s.get('https://cp.adidas.co.uk/web/eCom/en_GB/loadcreateaccount')
         soup = bs(res.text, 'lxml')
         recaptoken = fetchtoken()
         payload = {
             'firstName' : first,
             'lastName' : last,
-            'minAgeCheck' : 'true',
-            '_minAgeCheck' : 'on',
+            'day' : '4',
+            'month' : '4',
+            'year' : '1970',
             'email' : email,
             'password' : password,
             'confirmPassword' : password,
@@ -88,14 +89,19 @@ def createaccount(first, last, email, password):
             'g-recaptcha-response' : recaptoken,
             'metaAttrs[pageLoadedEarlier]':'true',
             'app':'eCom',
-            'locale':'en_US',
+            'locale':'en_GB',
             'domain':'',
-            'consentData1':'Sign me up for adidas emails, featuring exclusive offers, latest product info, news about upcoming events, and more. See our <a target="_blank" href="https://www.adidas.com/us/help-topics-privacy_policy.html">Privacy Policy</a> for details.',
-            'consentData2':'',
-            'consentData3':'',
+            'consentData1':'''I would like to stay up to date with adidas ''',
+            'consentData2':'''I agree to receiving personalised marketing messages about adidas products, events and promotions (including offers and discounts). adidas may contact me through the channels I select, such as email, SMS or post. <b><u>What does this mean?</u></b>''',
+            'consentData3':'''We, <a target="_blank" href="https://www.adidas.co.uk/help-topics-imprint.html">adidas International Trading B.V.</a>, or third parties on our behalf, may contact you with messages about adidas products, events and promotions or to ask your opinions when we conduct research. In order to provide you with the best personalised experience and to anticipate which of our products and services you might be interested in, we will create a profile based on the information we hold about you. To create this profile, we will store and analyse the personal data we have collected about you, including:
+- your name, date of birth and e-mail address. We may also store your telephone number or postal address  if you choose to be contacted by post or SMS;
+- your preferences and interests that either you have actively shared with us through your adidas account or accounts or those that we have inferred through your registered interactions with adidas websites and apps (for which we may use cookies ); and
+- your shopping history, both online and offline.
+We will keep the profiles we create secure and we will not share them with any third parties other than those that we engage to provide services on our behalf.
+You are in charge, meaning we may contact you only through the channels selected by you, such as email, telephone, apps, SMS or post. If you wish to unsubscribe or to opt out of a particular channel, please follow the steps contained in the particular message or contact <a target="_blank" href="https://www.adidas.co.uk/help">Customer Service</a>. For more information, including on how to exercise your rights in relation to the personal data we hold about you, please read our <a target="_blank" href="https://www.adidas.co.uk/help-topics-privacy_policy.html">Privacy Statement</a>.''',
             'CSRFToken': soup.find('input', {'name':'CSRFToken'})['value']
         }
-        res = s.post('https://cp.adidas.com/web/eCom/en_US/accountcreate', data = payload, headers = headers)
+        res = s.post('https://cp.adidas.co.uk/web/eCom/en_GB/accountcreate', data = payload, headers = headers)
         if 'spsessionauthnadapterid' not in res.text.lower():
             return(False)
         soup = bs(res.text, 'lxml')
@@ -112,12 +118,12 @@ def createaccount(first, last, email, password):
             "validator_id" : soup.find('input', {'name':'validator_id'})['value'],
             "app" : soup.find('input', {'name':'app'})['value']
         }
-        res = s.post('https://cp.adidas.com/idp/startSSO.ping', data = payload, headers = headers)
-        resumeurl = res.text[res.text.find('cp.adidas.com/idp')+13:res.text.find('resumeSAML20')+30]
-        resumeurldata = res.text[res.text.find('cp.adidas.com/idp')+13:res.text.find('resumeSAML20')+30].replace('/','%2f')
-        resumeurldata2 = 'eCom|en_US|cp.adidas.com|null'.replace('|', '%7C')
+        res = s.post('https://cp.adidas.co.uk/idp/startSSO.ping', data = payload, headers = headers)
+        resumeurl = res.text[res.text.find('cp.adidas.co.uk/idp')+15:res.text.find('resumeSAML20')+30]
+        resumeurldata = res.text[res.text.find('cp.adidas.co.uk/idp')+15:res.text.find('resumeSAML20')+30].replace('/','%2f')
+        resumeurldata2 = 'eCom|en_GB|cp.adidas.co.uk|null'.replace('|', '%7C')
         geturl = res.text[res.text.find('https://cp.adidasspecialty'):res.text.find('ssoiniturl')+32].replace('amp;','')
-        res = s.get('https://cp.adidas.com/web/ssoCookieCreate?' + resumeurldata + '&' + resumeurldata2, headers = headers)
+        res = s.get('https://cp.adidas.co.uk/web/ssoCookieCreate?' + resumeurldata + '&' + resumeurldata2, headers = headers)
         return(True)
     except Exception as f:
         print('Error : ' + f)
@@ -128,11 +134,11 @@ def login(email, passw):
         s = requests.Session()
         s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
         headers = {
-          'Origin':'https://cp.adidas.com',
-          'Referer':'https://cp.adidas.com',
+          'Origin':'https://cp.adidas.co.uk',
+          'Referer':'https://cp.adidas.co.uk',
           'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
         }
-        res = s.get('https://cp.adidas.com/web/eCom/en_US/loadsignin?target=account', headers = headers)
+        res = s.get('https://cp.adidas.co.uk/web/eCom/en_GB/loadsignin?target=account', headers = headers)
         soup = bs(res.text, 'lxml')
         payload = {
             'username': email,
@@ -157,28 +163,28 @@ def login(email, passw):
         payload['resumeURL_test'] = soup.find('input', {'name':'resumeURL_test'})['value']
         payload['FromFinishRegistraion'] = soup.find('input', {'name':'FromFinishRegistraion'})['value']
         payload['CSRFToken'] = soup.find('input', {'name':'CSRFToken'})['value']
-        res = s.post('https://cp.adidas.com/idp/startSSO.ping', data = payload, headers = headers)
+        res = s.post('https://cp.adidas.co.uk/idp/startSSO.ping', data = payload, headers = headers)
         sub = res.text[res.text.find('/idp/')+5:]
         firststring = sub[:sub.find('/idp/')]
         secondstring = sub[sub.find('/idp/')+5:sub.find("'")]
         url = '/idp/' + firststring + '/idp/' + secondstring
-        res = s.get('https://cp.adidas.com/web/ssoCookieCreate?resume={}&cd={}|{}|{}|null'.format(url.replace(' ', '%7C'), payload['cd'].replace(' ', '%7C'), payload['locale'].replace(' ', '%7C'), payload['domain'].replace(' ', '%7C')), headers = headers)
+        res = s.get('https://cp.adidas.co.uk/web/ssoCookieCreate?resume={}&cd={}|{}|{}|null'.format(url.replace(' ', '%7C'), payload['cd'].replace(' ', '%7C'), payload['locale'].replace(' ', '%7C'), payload['domain'].replace(' ', '%7C')), headers = headers)
         #soup = bs(res.text, 'lxml')
         #nexturl = soup.find('img')['src']
         #self.s.get(nexturl)
-        res = s.get('https://cp.adidas.com' + url, headers = headers)
+        res = s.get('https://cp.adidas.co.uk' + url, headers = headers)
         soup = bs(res.text, 'lxml')
         payload = {
             'RelayState':soup.find('input', {'name':'RelayState'})['value'],
             'SAMLResponse':soup.find('input', {'name': 'SAMLResponse'})['value']
         }
-        res = s.post('https://cp.adidas.com/sp/ACS.saml2', data = payload, headers = headers)
+        res = s.post('https://cp.adidas.co.uk/sp/ACS.saml2', data = payload, headers = headers)
         soup = bs(res.text, 'lxml')
         payload = {
             'REF':soup.find('input', {'name':'REF'})['value'],
             'TargetResource':soup.find('input', {'name':'TargetResource'})['value']
         }
-        res = s.post('https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-ResumeLogin', headers = headers, data = payload)
+        res = s.post('https://www.adidas.co.uk/on/demandware.store/Sites-adidas-GB-Site/en_GB/MyAccount-ResumeLogin', headers = headers, data = payload)
         return(True)
     except:
         return(False)
